d266c32fb13ae0925b60841ba641e0291a6629e6
work <138650726+alphabetf@users.noreply.github.com>
2024-05-22 08:19:40 +0800

    add
    add

---
+++ dht11_driver/dht11_drv.c
@@ -0,0 +1,149 @@
+#include "asm-generic/errno-base.h"
+#include "asm-generic/gpio.h"
+#include "linux/jiffies.h"
+#include <linux/module.h>
+#include <linux/poll.h>
+#include <linux/delay.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/miscdevice.h>
+#include <linux/kernel.h>
+#include <linux/major.h>
+#include <linux/mutex.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/stat.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/tty.h>
+#include <linux/kmod.h>
+#include <linux/gfp.h>
+#include <linux/gpio/consumer.h>
+#include <linux/platform_device.h>
+#include <linux/of_gpio.h>
+#include <linux/of_irq.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/slab.h>
+#include <linux/fcntl.h>
+#include <linux/timer.h>
+
+
+static int major = 0;
+static struct class *dht11_class;
+
+/* dht11 source description */
+struct dht11_desc{
+	unsigned int gpio;		/* led GPIO */
+	int 		 irq;		/* interrupt number */
+	char* 		 name;		/* led name */
+	int 		 num;		/* led number */
+	struct timer_list key_timer;
+};
+
+/* dht11  */
+/* A B C D E F G 6*16+2 */
+static struct led_desc gpio_dht11[1] = {
+	{10, 0, "mydht11_1", 0,},
+};
+
+ssize_t led_drv_read (struct file *file, char __user *buf, size_t size, loff_t *offset)
+{
+	char tmp_buf[2];
+    int err;
+	int count = sizeof(gpio_led)/sizeof(gpio_led[0]);
+	
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+	if(size != 2){
+		return -EINVAL;
+	}
+
+	err = copy_from_user(tmp_buf, buf, 1); /* read which gpio */
+	if(tmp_buf[0] >= count){
+		return -EINVAL;
+	}
+	tmp_buf[1] = gpio_get_value(gpio_led[(int)tmp_buf[0]].gpio);  /* get the gpio current status */
+
+	err = copy_to_user(buf, tmp_buf, 2);  /* return the gpio status */
+	
+	return 2;
+}
+
+ssize_t led_drv_write (struct file *file, const char __user *buf, size_t size, loff_t *offset)
+{
+	unsigned char ker_buf[2];
+	int err;
+
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+	if(size != 2){
+		return -EINVAL;
+	}
+
+	err = copy_from_user(ker_buf, buf, size);
+	if(ker_buf[0] >= sizeof(gpio_led)/sizeof(gpio_led[0])){
+		return -EINVAL;
+	}
+
+	gpio_set_value(gpio_led[ker_buf[0]].gpio, ker_buf[1]);	/* set gpio value */
+	
+	return 2;
+}
+
+static struct file_operations gpio_led_drv_fops = {
+	.owner = THIS_MODULE,
+	.read    = led_drv_read,
+	.write   = led_drv_write,
+};
+
+/* entry */
+static int __init dht11_drv_init(void)
+{
+	int err;
+	int i;
+	int count = sizeof(gpio_dht11)/sizeof(gpio_dht11[0]);
+	
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+	for(i = 0; i < count; i++){ /* set the GPIO */
+		err = gpio_request(gpio_led[i].gpio, gpio_led[i].name);
+		if(err < 0){
+			printk("can not request gpio %s %d\n",gpio_led[i].name, gpio_led[i].gpio);
+			return -ENODEV;
+		}
+		gpio_direction_output(gpio_led[i].gpio, 1); /* gpio output */
+	}
+
+    /* register file operator */
+	major = register_chrdev(0, "myled",&gpio_led_drv_fops);
+	led_class = class_create(THIS_MODULE, "gpio_led_class");
+	if(IS_ERR(led_class)){
+		printk("%s %s line %d \n", __FILE__, __FUNCTION__, __LINE__);
+		unregister_chrdev(major, "myled");
+		return PTR_ERR(led_class);
+	}
+	/* create dev node */
+	device_create(led_class, NULL, MKDEV(major, 0), NULL, "myled_dev");
+
+	return err;
+}
+ 
+/* exit */
+static void __exit dht11_drv_exit(void)
+{
+	int i;
+	int count = sizeof(gpio_dht11)/sizeof(gpio_dht11[0]);
+
+	for(i = 0; i < count; i++){
+		gpio_free(gpio_led[i].gpio);	/* release the gpio */ 
+	}
+
+	device_destroy(led_class, MKDEV(major, 0));
+	class_destroy(led_class);
+	unregister_chrdev(major, "myled");
+}
+
+module_init(dht11_drv_init);
+module_exit(dht11_drv_exit);
+MODULE_LICENSE("GPL");
---
+++ dht11_driver/dht11_test.c
@@ -0,0 +1,50 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <poll.h>
+#include <signal.h>
+
+static int fd;
+
+/*
+ * ./dht11 /dev/mydht11
+ *
+ */
+int main(int argc, char **argv)
+{
+	char buf[4];
+	int ret;
+
+	int i;
+	
+	/* 1. 判断参数 */
+	if (argc != 2){
+		printf("Usage: %s <dev>\n", argv[0]);
+		return -1;
+	}
+
+	/* 2. 打开文件 */
+	fd = open(argv[1], O_RDWR | O_NONBLOCK);
+	if (fd == -1){
+		printf("can not open file %s\n", argv[1]);
+		return -1;
+	}
+
+	while (1){
+		if (read(fd, buf, 4) == 4){
+			printf("get Humidity: %d:%d, Temperature : %d:%d\n", buf[0],buf[1],buf[2],buf[3]);
+		}else{
+			printf("get dht11: -1\n");
+		}
+		sleep(5);
+	}
+
+	close(fd);
+	
+	return 0;
+}
+
+
